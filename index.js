dcfa8ab4b2184b0c151d8e1521b64ef62d7c83e0
Your Name <you@example.com>
2026-06-28 20:18:06 +0530

    adding

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/world', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our server!');
+});
+
+
+app.get('/home', (req, res) => {
+  res.send('Welcome to our HomePage!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
